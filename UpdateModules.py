ba5072c588df45b302dc50246cd3b6067c2eb922
Sergey Dokin <sergey@dokin.ru>
2024-05-17 11:51:40 +0300

    1.1.3

--- UpdateModules.py
+++ UpdateModules.py
@@ -1,13 +1,11 @@
 import os
 import traceback
 from rich import print
-# from threading import Thread
-from time import perf_counter#,sleep
+from time import perf_counter,sleep
 from platform import python_version
 from subprocess import Popen, PIPE,TimeoutExpired#,SubprocessError
-__version__ = '1.1.0'
-__verdate__ = '2024-05-16 20:15'
-gcmdres:str = ''
+__version__ = '1.1.3'
+__verdate__ = '2024-05-17 11:43'
 
 class DokExcept(Exception):
     def __init__(self, message:str):
@@ -19,8 +17,15 @@ def cmdexec(CMDcom:list)->str:
     cmdres = ''
     try:
     #---------------------------------------------------------------------------------------------------------------------
-        with Popen(CMDcom, shell=True, stdout = PIPE, stderr = PIPE, encoding="cp866") as popps:
-            pls = popps.communicate(timeout=60)
+        print('[', end = '', flush=True)
+        with Popen(CMDcom, shell=True,stdout = PIPE,stderr = PIPE, encoding="cp866") as popps:
+            for ici in range(100):
+                if popps.poll():break
+                if not ici % 10:print('[green]#', end = '', flush=True)
+                sleep(0.1)
+            print(f'] {(ici+1)/10:.2f} c')
+            pls = popps.communicate()
+        # print(f'Результат:{pls}')
         if bool(pls[0]):cmdres = str(pls[0])
         if bool(pls[1]):raise DokExcept (pls[1])
     #----------------------------------------------------------------------------------------------------------------------
@@ -33,6 +38,18 @@ def cmdexec(CMDcom:list)->str:
         print(f'[orchid]{traceback.format_exc()}')
     return cmdres
 ###########################################################################################################################
+def cmdexecNoOut(CMDcom:list)->str:
+    try:
+    #---------------------------------------------------------------------------------------------------------------------,stdout = PIPE,stderr = PIPE
+        with Popen(CMDcom, shell=True, encoding="cp866") as popps:
+            popps.communicate(120)
+    #----------------------------------------------------------------------------------------------------------------------
+    except TimeoutExpired as Mess:
+        print(f'[yellow]Тиймаут:{Mess}')
+    except Exception as MErs:
+        print(f'[orchid]{MErs}')
+        print(f'[orchid]{traceback.format_exc()}')
+###########################################################################################################################
 if __name__ == '__main__':
     print(f'Обновление модулей Python ver: {python_version()}')
     start_time = tick_time_1 = tick_time_2 = perf_counter()
@@ -43,7 +60,7 @@ if __name__ == '__main__':
     # print(pls[1].decode('cp866'))
     if  rez:
         restxt = rez.splitlines()[2:]
-        print(f'Доступны для обновления:2')
+        print(f'Доступны для обновления:{len(restxt)}')
         for ipr in restxt:
             print(f'\t{ipr}')
         tick_time_1 = perf_counter()
@@ -54,7 +71,8 @@ if __name__ == '__main__':
                 tprg = it.split()[0]
                 print(f'Модуль {tprg} версии {it.split()[1]}')
                 cmdlist = ['pip','install','-U',f'{tprg}']
-                # cmdexec(cmdlist)
+                # cmdlist = ['timeout','/t','5','/nobreak']# >nul
+                cmdexecNoOut(cmdlist)
     else:
         tick_time_2 = perf_counter()
         print('Все модули последней версии')
@@ -62,5 +80,5 @@ if __name__ == '__main__':
     stop_time = perf_counter()
     Inetrval_1 = tick_time_1 - start_time
     Inetrval_2 = stop_time - tick_time_2
-    input(f'\n{Inetrval_1:.>5} : {Inetrval_2:.>5} :-> ')
+    input(f'\n{Inetrval_1:.5f} : {Inetrval_2:.5f} :-> ')
     os._exit(0)
